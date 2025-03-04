5cb2269d170240b0a0dacf2197f3be58f0d6c491
kalpesh kadam <kalpesh.k@kwebmaker.com>
2025-03-04 18:25:08 +0530

    Footer Responsive

--- components/Layout/Footer/Footer.jsx
+++ components/Layout/Footer/Footer.jsx
@@ -1,143 +1,85 @@
 import SubscribeNewsletter from "@/components/Ui/SubscribeNewsletter";
 import Link from "next/link";
-import { useState } from "react";
 import Image from "next/image";
 import SocialLinks from "@/components/Ui/SocialLinks";
 
 const quickLinks = [
-  {
-    name:'About Us',
-    url:'#'
-  },
-  {
-    name:'Advertise',
-    url:'#'
-  },
-  {
-    name:'Editorial',
-    url:'#'
-  },
-  {
-    name:'Media Kit',
-    url:'#'
-  },
+  { name: "About Us", url: "#" },
+  { name: "Advertise", url: "#" },
+  { name: "Editorial", url: "#" },
+  { name: "Media Kit", url: "#" },
+  { name: "Magazine", url: "#" },
+  { name: "Events", url: "#" },
+  { name: "Subscription", url: "#" },
+  { name: "Careers", url: "#" },
+  { name: "Contact Us", url: "#" },
+];
 
-  {
-    name:'Magazine',
-    url:'#'
-  },
-  {
-    name:'Events',
-    url:'#'
-  },
-  {
-    name:'Subscription',
-    url:'#'
-  },
-  {
-    name:'Careers',
-    url:'#'
-  },
-  {
-    name:'Contact Us',
-    url:'#'
-  },
-]
 const categoryLinks = [
-  {
-    name:'Leadership',
-    url:'#'
-  },
-  {
-    name:'Business',
-    url:'#'
-  },
-  {
-    name:'The Brand Edit',
-    url:'#'
-  },
-  {
-    name:'Architecture + Design',
-    url:'#'
-  },
-  {
-    name:'Voyages of Influence',
-    url:'#'
-  },
-  {
-    name:'Travel',
-    url:'#'
-  },
-  {
-    name:'Dine & Drink',
-    url:'#'
-  },
-  {
-    name:'SOH Loves',
-    url:'#'
-  },
-  {
-    name:'Videos',
-    url:'#'
-  },
-]
+  { name: "Leadership", url: "#" },
+  { name: "Business", url: "#" },
+  { name: "The Brand Edit", url: "#" },
+  { name: "Architecture + Design", url: "#" },
+  { name: "Voyages of Influence", url: "#" },
+  { name: "Travel", url: "#" },
+  { name: "Dine & Drink", url: "#" },
+  { name: "SOH Loves", url: "#" },
+  { name: "Videos", url: "#" },
+];
+
+const currentYear = new Date().getFullYear();
 
 const Footer = () => {
-
-  const [year, setYear] = useState(new Date().getFullYear());
-
   return (
-    <footer className="bg-[#F7F3EA] xl:pt-[5.208vw] relative">
-      {/* <div className="whiteStar xl:right-[4.688vw]"></div>
+    <footer className="bg-[#F7F3EA] pt-[50px] xl:pt-[5.208vw] relative">
+      <div className="whiteStar right-[10%]"></div>
       <div className="container xl:px-[4.167vw]">
-
-        <div className="row justify-between">
-          <div className="xl:w-[27.396vw]">
-            <div className="xl:mb-[5.208vw]">
-              <SubscribeNewsletter/>
-            </div>
-            <div className="row">
-              <div className="flex-none"></div>
-              <div className="flex-1">
-                <div className="flex items-center xl:gap-x-[1vw]">
-                  <div className="flex-none"><h6 className="font-fh text-black xl:text-[1.042vw]">Follow Us On:</h6></div>
-                  <div className="flex-1"><SocialLinks/></div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="md:flex justify-between *:mb-[30px] *:md:mb-0">
+          {/* Links Section */}
           <div className="xl:w-[25%]">
             <div className="row justify-center xl:gap-x-[3vw]">
-              <ul className="flex-1 *:mb-[1vw]">
-                {
-                  quickLinks.map((item,index)=>
-                  <li key={index}><Link href={item.url} className="text-black hover:font-semibold hover:text-gold">{item.name}</Link></li>
-                  )
-                }
-              </ul>
-              <ul className="flex-1 *:mb-[1vw]">
-                {
-                  categoryLinks.map((item,index)=>
-                  <li key={index}><Link href={item.url} className="text-black hover:font-semibold hover:text-gold">{item.name}</Link></li>
-                  )
-                }
-              </ul>
+              {[quickLinks, categoryLinks].map((links, i) => (
+                <ul key={i} className="flex-1 *:mb-[10px] *:xl:mb-[1vw]">
+                  {links.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className="text-black hover:font-semibold hover:text-gold">
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ))}
             </div>
           </div>
-          <div className="xl:w-[14.583vw] self-end">
-            <Image src={'/images/soh-foot-logo.svg'} width={280} height={185}/>
+
+          {/* Newsletter & Social Links */}
+          <div className="xl:w-[27.396vw] md:order-first">
+            <div className="mb-[30px] xl:mb-[7vw]">
+              <SubscribeNewsletter />
+            </div>
+            <div className="flex items-center gap-x-[10px] xl:gap-x-[1vw]">
+              <h6 className="font-fh text-black text-[16px] xl:text-[1.042vw] flex-none">Follow Us On:</h6>
+              <SocialLinks />
+            </div>
+          </div>
+
+          {/* Logo Section */}
+          <div className="w-[100px] xl:w-[14.583vw] self-end">
+            <Image src="/images/soh-foot-logo.svg" width={280} height={185} alt="SOH Footer Logo" />
           </div>
         </div>
 
-        <div className="row justify-between border-t-[1px] border-solid border-[#C2C2C2] xl:text-[0.833vw] xl:py-[2.604vw] xl:mt-[3.125vw]">
+        {/* Footer Bottom */}
+        <div className="lg:flex text-center justify-between border-t border-[#C2C2C2] text-[12px] xl:text-[0.833vw] py-[10px] lg:py-[20px] xl:py-[2.604vw] mt-[20px] xl:mt-[3.125vw] sm:*:mb-[5px] last:*:mb-0 lg:*:mb-0">
           <div className="flex-none">
-            © {year} SHO Vitrified Pvt Ltd. All Rights Reserved | Designed by <Link href="#">kwebmaker</Link>
+            © {currentYear} SHO Vitrified Pvt Ltd. <br className="sm:hidden" /> All Rights Reserved | Designed by{" "}
+            <Link href="#">kwebmaker</Link>
           </div>
           <div className="flex-none">
             <Link href="#">Privacy Policy</Link> | <Link href="#">Terms of Service</Link>
           </div>
         </div>
-      </div> */}
+      </div>
     </footer>
   );
 };
--- components/Ui/SubscribeNewsletter.jsx
+++ components/Ui/SubscribeNewsletter.jsx
@@ -3,10 +3,10 @@ import InputField from "./Forms/InputField"
 const SubscribeNewsletter = () => {
   return (
     <>
-      <h4 className="text-black xl:mr-[7.813vw] font-fh xl:text-[1.667vw] xl:mb-[4.167vw]">Subscribe for our Newsletters</h4>
-      <div className="row border-b border-solid border-[#000] xl:gap-x-[1vw]">
+      <h4 className="text-black xl:mr-[7.813vw] font-fh text-[22px] xl:text-[1.667vw] mb-[15px] xl:mb-[4.167vw]">Subscribe for our Newsletters</h4>
+      <div className="flex border-b border-solid border-[#000] xl:gap-x-[1vw]">
         <InputField type={'email'} placeholder={'Enter Email ID'} customClass={'flex-1 xl:text-[0.833vw] placeholder:text-black text-black'}/>
-        <button className="flex-none xl:w-[2.5vw] xl:h-[2.5vw]">
+        <button className="flex-none w-[30px] h-[30px] xl:w-[2.5vw] xl:h-[2.5vw]">
             <svg viewBox="0 0 48 48" fill="#000" className="w-full h-full">
                 <path d="M38.1575 25.0002H9C8.71533 25.0002 8.4775 24.9047 8.2865 24.7137C8.0955 24.5227 8 24.2849 8 24.0002C8 23.7155 8.0955 23.4777 8.2865 23.2867C8.4775 23.0957 8.71533 23.0002 9 23.0002H38.1575L33.677 18.5077C33.482 18.313 33.38 18.0835 33.371 17.8192C33.362 17.5552 33.4678 17.313 33.6885 17.0927C33.9088 16.8797 34.1467 16.7714 34.402 16.7677C34.657 16.7637 34.891 16.872 35.104 17.0927L40.869 22.8692C41.1923 23.1925 41.354 23.5695 41.354 24.0002C41.354 24.4309 41.1923 24.8079 40.869 25.1312L35.0925 30.9077C34.9052 31.095 34.6775 31.195 34.4095 31.2077C34.1415 31.2207 33.8973 31.1207 33.677 30.9077C33.4563 30.6874 33.346 30.4495 33.346 30.1942C33.346 29.9392 33.4563 29.7015 33.677 29.4812L38.1575 25.0002Z"/>
             </svg>
