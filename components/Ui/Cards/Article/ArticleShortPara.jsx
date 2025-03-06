aa054bf21123fd080496b549c64d2e3ee68ccd07
kalpesh kadam <kalpesh.k@kwebmaker.com>
2025-03-06 12:56:04 +0530

    mobile font size decrease of article card

--- components/Ui/Cards/Article/ArticleShortPara.jsx
+++ components/Ui/Cards/Article/ArticleShortPara.jsx
@@ -1,7 +1,7 @@
 
 const ArticleShortPara = ({articleShortPara, paraStyle}) => {
   return (
-    <p className={`overflow-hidden text-ellipsis line-clamp-3 mb-[10px] xl:!mb-[.5vw] ${paraStyle ? paraStyle : ''}`}>
+    <p className={`overflow-hidden text-ellipsis line-clamp-3 mb-[10px] text-[12px] sm:text-[14px] xl:!mb-[.5vw] ${paraStyle ? paraStyle : ''}`}>
       {articleShortPara && articleShortPara}
     </p>
   )
--- components/Ui/Cards/Article/ArticleTitle.jsx
+++ components/Ui/Cards/Article/ArticleTitle.jsx
@@ -5,7 +5,7 @@ const ArticleTitle = ({articleSubCategory, articleTitle, customClass}) => {
     <div>
       <span className={`block text-[#99742B] font-medium text-[12px] xl:mb-[0.521vw] xl:text-[0.729vw] uppercase`}>{articleSubCategory && articleSubCategory}</span>
       <h6
-      className={`text-mainBlack font-medium overflow-hidden text-ellipsis line-clamp-2 text-[16px] xl:text-[1.042vw] mb-[10px] xl:mb-[0.521vw] ${
+      className={`text-mainBlack font-medium overflow-hidden text-ellipsis line-clamp-2 sm:text-[16px] xl:text-[1.042vw] mb-[10px] xl:mb-[0.521vw] ${
         customClass && customClass
       }`}
     >
