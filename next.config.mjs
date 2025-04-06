20b8a0ed1093423026c86f9cb4ed063f29cd0713
manojbhandi <manojkumar@kwebmaker.com>
2025-04-06 16:08:49 +0530

    next config chnaged

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // images: {
+  //   unoptimized: true,
+  // },
   images: {
-    unoptimized: true,
+    domains: ["134.209.150.140"],
   },
   transpilePackages: ["gsap"],
 };
