55db244db29f8683495bde2ed7e2e172f496b89e
manojbhandi <manojkumar@kwebmaker.com>
2025-04-07 16:58:08 +0530

    ui issues

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,24 @@ import HomePage from "@/components/Home/HomePage";
 import { API_END_POINTS } from "@/utils/constants";
 import { fetchPageData } from "@/utils/fetchPageData";
 
-const Index = ({ banners, homeData }) => {
-  return <HomePage data={homeData} banners={banners} />;
+const Index = ({ banners, homeData, categories }) => {
+  return <HomePage data={homeData} banners={banners} categories={categories} />;
 };
 
 export default Index;
 
 export const getServerSideProps = async () => {
-  const [homeData, banners] = await Promise.all([
+  const [homeData, banners, categories] = await Promise.all([
     fetchPageData(API_END_POINTS.home),
     fetchPageData(API_END_POINTS.getHomeBanners),
+    fetchPageData(API_END_POINTS.getAllCatgories),
   ]);
 
   return {
     props: {
       banners: banners?.props?.data,
       homeData: homeData?.props?.data,
+      categories: categories?.props?.data,
     },
   };
 };
